67a9ff29dc7700a2121674b782bc846aca9b4ad5
Daniel Martin <daniel.martin@klar-automation.de>
2019-11-26 14:49:02 +0100

    Travis: Using shell script for publishing

---
+++ ci_npm_publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd coreclr-hosting
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+if [ "$TRAVIS_TAG" != "" ]
+then
+	echo "Publishing coreclr-hosting for tag $TRAVIS_TAG"
+    npm version $TRAVIS_TAG
+	npm publish --access public --ignore-scripts
+else
+    echo "Dry run publishing coreclr-hosting for tag $TRAVIS_TAG"
+    npm publish --access public --ignore-scripts --dry-run
+fi
+
+cd ../electron-blazor-glue
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+if [ "$TRAVIS_TAG" != "" ]
+then
+	echo "Publishing electron-blazor-glue for tag $TRAVIS_TAG"
+    npm uninstall coreclr-hosting --ignore-scripts  
+    npm install @nodeclrhosting/coreclr-hosting@$TRAVIS_TAG --ignore-scripts  
+    npm version $TRAVIS_TAG
+	npm publish --access public --ignore-scripts
+else
+    echo "Dry run publishing electron-blazor-glue for tag $TRAVIS_TAG"
+    npm publish --access public --ignore-scripts --dry-run
+fi
+
+cd ..
