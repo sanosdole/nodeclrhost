ee5172537fdc97b6b8ae68ec90d1ce1182ce69e2
Daniel Martin <daniel.martin@klar-automation.de>
2019-11-26 18:04:41 +0100

    Use v as release prefix to obey prebuild convention

--- ci_npm_publish.sh
+++ ci_npm_publish.sh
@@ -5,11 +5,11 @@ echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
 if [ "$TRAVIS_TAG" != "" ]
 then
-	echo "Publishing coreclr-hosting for tag $TRAVIS_TAG"
-    npm version $TRAVIS_TAG --allow-same-version
+	echo "Publishing coreclr-hosting for tag ""${TRAVIS_TAG:1}"
+    npm version ${TRAVIS_TAG:1} --allow-same-version
 	npm publish --access public --ignore-scripts
 else
-    echo "Dry run publishing coreclr-hosting for tag $TRAVIS_TAG"
+    echo "Dry run publishing coreclr-hosting"
     npm publish --access public --ignore-scripts --dry-run
 fi
 
@@ -21,11 +21,11 @@ then
 	echo "Publishing electron-blazor-glue for tag $TRAVIS_TAG"
     # npm uninstall coreclr-hosting --ignore-scripts
     # npm install @nodeclrhosting/coreclr-hosting@$TRAVIS_TAG --ignore-scripts
-    sed -i 's!"coreclr-hosting": "file:../coreclr-hosting"!"coreclr-hosting": "'"$TRAVIS_TAG"'"!g' package.json
-    npm version $TRAVIS_TAG --allow-same-version
+    sed -i 's!"coreclr-hosting": "file:../coreclr-hosting"!"coreclr-hosting": "'"${TRAVIS_TAG:1}"'"!g' package.json
+    npm version ${TRAVIS_TAG:1} --allow-same-version
 	npm publish --access public --ignore-scripts
 else
-    echo "Dry run publishing electron-blazor-glue for tag $TRAVIS_TAG"
+    echo "Dry run publishing electron-blazor-glue"
     npm publish --access public --ignore-scripts --dry-run
 fi
 
