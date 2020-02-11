0cb062376ae843bab0efb8bbb4fd69b182e730c2
Daniel Martin <daniel.martin@klar-automation.de>
2020-02-11 18:31:26 +0100

    We pass the path to the node module using the environment variable `CORECLR_HOSTING_MODULE_PATH`

--- coreclr-hosting/bindings.js
+++ coreclr-hosting/bindings.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const process = require('process');
 
 var nativeModulePath = path.join(__dirname, 'build', process.env.DEBUG ? 'Debug' : 'Release', 'coreclr-hosting.node');
 //console.log("Loading coreclr-hosting.node from " + nativeModulePath);
+process.env.CORECLR_HOSTING_MODULE_PATH = nativeModulePath;
 var coreclrHosting = require(nativeModulePath);
 
 module.exports = coreclrHosting;
