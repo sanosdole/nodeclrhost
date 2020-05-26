bce4e2692b218d14a5b5f6f92375d3aa8388488a
Daniel Martin <daniel.martin@klar-automation.de>
2020-05-27 01:22:24 +0200

    Just some cleanup & comments

--- examples/electron-mvc/MvcApp/Program.cs
+++ examples/electron-mvc/MvcApp/Program.cs
@@ -1,10 +1,6 @@
 namespace MvcApp
 {
    using System;
-   using System.Diagnostics;
-   using System.IO;
-   using System.Linq;
-   using System.Reflection;
    using System.Threading;
    using System.Threading.Tasks;
    using Microsoft.AspNetCore.Hosting;
@@ -14,6 +10,7 @@ namespace MvcApp
    public class Program
    {
       private static dynamic browserWindow;
+
       public static async Task Main(string[] args)
       {
          //if (args.Any(arg => arg.Equals("-debug")))
@@ -25,34 +22,35 @@ namespace MvcApp
          {
             using var stopSource = new CancellationTokenSource();
             var electron = node.Global.electron;
-            electron.app.on("ready", new Action<dynamic>((dynamic launchInfo) =>
-                                                         {
-                                                            var options = node.New();
-                                                            options.title = ".NET rocks";
-                                                            options.backgroundColor = "#fff";
-                                                            options.useContentSize = true;
-                                                            //options.kiosk = true;
+            electron.app.on("ready",
+                            new Action<dynamic>((dynamic launchInfo) =>
+                                                {
+                                                   var options = node.New();
+                                                   options.title = ".NET rocks";
+                                                   options.backgroundColor = "#fff";
+                                                   options.useContentSize = true;
+                                                   //options.kiosk = true;
 
-                                                            var webPreferences = node.New();
-                                                            options.webPreferences = webPreferences;
-                                                            webPreferences.nodeIntegration = true;
-                                                            /*webPreferences.contextIsolation = false;
-                                                               webPreferences.sandbox = false;
-                                                               webPreferences.devTools = false;*/
+                                                   var webPreferences = node.New();
+                                                   options.webPreferences = webPreferences;
+                                                   webPreferences.nodeIntegration = true;
+                                                   /*webPreferences.contextIsolation = false;
+                                                      webPreferences.sandbox = false;
+                                                      webPreferences.devTools = false;*/
 
-                                                            //console.log("options:", options);
+                                                   //console.log("options:", options);
 
-                                                            browserWindow = electron.BrowserWindow.CreateNewInstance(options);
+                                                   browserWindow = electron.BrowserWindow.CreateNewInstance(options);
 
-                                                            browserWindow.loadURL("https://localhost:5001/");
+                                                   browserWindow.loadURL("https://localhost:5001/");
+                                                }));
 
-                                                         }));
-
-            electron.app.on("will-quit", new Action<dynamic>(e =>
-                                                             {
-                                                                stopSource.Cancel();
-                                                                e.preventDefault();
-                                                             }));
+            electron.app.on("will-quit",
+                            new Action<dynamic>(e =>
+                                                {
+                                                   stopSource.Cancel();
+                                                   e.preventDefault();
+                                                }));
 
             var webHost = CreateHostBuilder(args).Build();
             await webHost.RunAsync(token: stopSource.Token);
@@ -61,12 +59,13 @@ namespace MvcApp
          {
             Console.WriteLine(e);
          }
+
          node.Global.process.exit();
       }
 
       public static IHostBuilder CreateHostBuilder(string[] args) =>
          Host.CreateDefaultBuilder(args)
-             .UseContentRoot(args[0]) // TODO DM 27.05.2020: Why is this necessary?
+             .UseContentRoot(args[0]) // DM 27.05.2020: This is necessary as static files are based on current directory
              .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
    }
 }
